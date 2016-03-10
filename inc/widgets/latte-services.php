a9fc90e742abab7a130e555cfc6eff54391da323
hardeepasrani <hardeepasrani@gmail.com>
2016-03-10 21:25:28 +0530

    Escaped HTML attibutes.

--- inc/widgets/latte-services.php
+++ inc/widgets/latte-services.php
@@ -22,30 +22,30 @@ class latte_services_widget extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, array( 'icon' => 'fa-glass' ));
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php esc_html_e('Title', 'latte'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php if( !empty($instance['title']) ): echo esc_html( $instance['title'] ); endif; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id('title') ); ?>"><?php esc_html_e('Title', 'latte'); ?></label> 
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id('title') ); ?>" name="<?php echo esc_attr( $this->get_field_name('title') ); ?>" type="text" value="<?php if( !empty($instance['title']) ): echo esc_html( $instance['title'] ); endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('icon'); ?>"><?php esc_html_e('Icon', 'latte'); ?></label>
-			<select class='widefat' id="<?php echo $this->get_field_id('icon'); ?>" name="<?php echo $this->get_field_name('icon'); ?>">
+			<label for="<?php echo esc_attr( $this->get_field_id('icon') ); ?>"><?php esc_html_e('Icon', 'latte'); ?></label>
+			<select class='widefat' id="<?php echo esc_attr( $this->get_field_id('icon') ); ?>" name="<?php echo esc_attr( $this->get_field_name('icon') ); ?>">
 			<?php foreach($fontawesome as $item): ?>
-				<option value="<?php echo $item; ?>" <?php if ($instance['icon'] == $item) echo 'selected="selected"'; ?>><?php echo esc_html($item); ?></option>
+				<option value="<?php echo esc_html($item); ?>" <?php if ($instance['icon'] == $item) echo 'selected="selected"'; ?>><?php echo esc_html($item); ?></option>
 			<?php endforeach; ?>
 			</select>
 			<label><a target="_blank" href="http://fortawesome.github.io/Font-Awesome/icons/"><?php _e('Click here to see a list of icons.', 'latte'); ?></a></label>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('link'); ?>"><?php esc_html_e('Link', 'latte'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>" type="url" value="<?php if( !empty($instance['link']) ): echo esc_url($instance['link']); endif; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id('link') ); ?>"><?php esc_html_e('Link', 'latte'); ?></label> 
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id('link') ); ?>" name="<?php echo esc_attr( $this->get_field_name('link') ); ?>" type="url" value="<?php if( !empty($instance['link']) ): echo esc_url($instance['link']); endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('newwindow'); ?>"><?php esc_html_e('Open In New Window?', 'latte'); ?></label> 
-			<input type="hidden" name="<?php echo $this->get_field_name('newwindow'); ?>" value="0" />
-			<input id="<?php echo $this->get_field_id('newwindow'); ?>" name="<?php echo $this->get_field_name('newwindow'); ?>" type="checkbox" value="1" <?php if (isset($instance['newwindow'])) : if (1 == $instance['newwindow']) echo 'checked="checked"'; endif ?> />
+			<label for="<?php echo esc_attr( $this->get_field_id('newwindow') ); ?>"><?php esc_html_e('Open In New Window?', 'latte'); ?></label> 
+			<input type="hidden" name="<?php echo esc_attr( $this->get_field_name('newwindow') ); ?>" value="0" />
+			<input id="<?php echo esc_attr( $this->get_field_id('newwindow') ); ?>" name="<?php echo esc_attr( $this->get_field_name('newwindow') ); ?>" type="checkbox" value="1" <?php if (isset($instance['newwindow'])) : if (1 == $instance['newwindow']) echo 'checked="checked"'; endif ?> />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('text'); ?>"><?php esc_html_e('Text', 'latte'); ?></label> 
-			<textarea class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php if( !empty($instance['text']) ): echo wp_kses_post( force_balance_tags( $instance['text'] ) ); endif; ?></textarea>
+			<label for="<?php echo esc_attr( $this->get_field_id('text') ); ?>"><?php esc_html_e('Text', 'latte'); ?></label> 
+			<textarea class="widefat" id="<?php echo esc_attr( $this->get_field_id('text') ); ?>" name="<?php echo esc_attr( $this->get_field_name('text') ); ?>"><?php if( !empty($instance['text']) ): echo wp_kses_post( force_balance_tags( $instance['text'] ) ); endif; ?></textarea>
 		</p>
 		<?php 
 	}
--- inc/widgets/latte-skills.php
+++ inc/widgets/latte-skills.php
@@ -20,27 +20,27 @@ class latte_skills_widget extends WP_Widget {
 	function form($instance) {
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php esc_html_e('Skill', 'latte'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php if( !empty($instance['title']) ): echo esc_html($instance['title']); endif; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id('title') ); ?>"><?php esc_html_e('Skill', 'latte'); ?></label> 
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id('title') ); ?>" name="<?php echo esc_attr( $this->get_field_name('title') ); ?>" type="text" value="<?php if( !empty($instance['title']) ): echo esc_html($instance['title']); endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('percentage'); ?>"><?php esc_html_e('Percentage', 'latte'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('percentage'); ?>" name="<?php echo $this->get_field_name('percentage'); ?>" type="number" min="0" max="100" value="<?php if( !empty($instance['percentage']) ): echo intval($instance['percentage']); endif; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id('percentage') ); ?>"><?php esc_html_e('Percentage', 'latte'); ?></label> 
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id('percentage') ); ?>" name="<?php echo esc_attr( $this->get_field_name('percentage') ); ?>" type="number" min="0" max="100" value="<?php if( !empty($instance['percentage']) ): echo intval($instance['percentage']); endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('titlecolor'); ?>"><?php esc_html_e('Title Color', 'latte'); ?></label> 
+			<label for="<?php echo esc_attr( $this->get_field_id('titlecolor') ); ?>"><?php esc_html_e('Title Color', 'latte'); ?></label> 
 			<br/>
-			<input id="<?php echo $this->get_field_id('titlecolor'); ?>" name="<?php echo $this->get_field_name('titlecolor'); ?>" type="color" data-default-color="#FFF" value="<?php if( !empty($instance['titlecolor']) ): echo esc_html($instance['titlecolor']); else: echo'#FFFFFF'; endif; ?>" />
+			<input id="<?php echo esc_attr( $this->get_field_id('titlecolor') ); ?>" name="<?php echo esc_attr( $this->get_field_name('titlecolor') ); ?>" type="color" data-default-color="#FFF" value="<?php if( !empty($instance['titlecolor']) ): echo esc_html($instance['titlecolor']); else: echo'#FFFFFF'; endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('titlebackground'); ?>"><?php esc_html_e('Title Background', 'latte'); ?></label> 
+			<label for="<?php echo esc_attr( $this->get_field_id('titlebackground') ); ?>"><?php esc_html_e('Title Background', 'latte'); ?></label> 
 			<br/>
-			<input id="<?php echo $this->get_field_id('titlebackground'); ?>" name="<?php echo $this->get_field_name('titlebackground'); ?>" type="color" data-default-color="#D35400" value="<?php if( !empty($instance['titlebackground']) ): echo esc_html($instance['titlebackground']); else: echo'#D35400'; endif; ?>" />
+			<input id="<?php echo esc_attr( $this->get_field_id('titlebackground') ); ?>" name="<?php echo esc_attr( $this->get_field_name('titlebackground') ); ?>" type="color" data-default-color="#D35400" value="<?php if( !empty($instance['titlebackground']) ): echo esc_html($instance['titlebackground']); else: echo'#D35400'; endif; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('barbackground'); ?>"><?php esc_html_e('Bar Background', 'latte'); ?></label> 
+			<label for="<?php echo esc_attr( $this->get_field_id('barbackground') ); ?>"><?php esc_html_e('Bar Background', 'latte'); ?></label> 
 			<br/>
-			<input id="<?php echo $this->get_field_id('barbackground'); ?>" name="<?php echo $this->get_field_name('barbackground'); ?>" type="color" data-default-color="#E67E22" value="<?php if( !empty($instance['barbackground']) ): echo esc_html($instance['barbackground']); else: echo'#E67E22'; endif; ?>" />
+			<input id="<?php echo esc_attr( $this->get_field_id('barbackground') ); ?>" name="<?php echo esc_attr( $this->get_field_name('barbackground') ); ?>" type="color" data-default-color="#E67E22" value="<?php if( !empty($instance['barbackground']) ): echo esc_html($instance['barbackground']); else: echo'#E67E22'; endif; ?>" />
 		</p>
 		<?php 
 	}
